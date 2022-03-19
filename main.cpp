cae7d9b7fc7d98062b3f96927ac33ab4f1623748
xovest <79008917+xovest@users.noreply.github.com>
2022-03-19 23:41:31 +0200

    just the command thingy

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 int main() {
-  Planet *p = 0;
-  insertEnd(p, "one");
-  insertEnd(p, "two");
-  insertEnd(p, "three");
-  insertEnd(p, "four");
-  insertEnd(p, "five");
-  insertEnd(p, "six");
-  insertAtLocation(p, "intruder", 0);
-  display(p);
+  //splitting the command
+  char command[100];
+  cin.get(command, 100);
+  char* token = strtok(command, " ");
+  string s[10];
+  int k = 0;
+  while (token != NULL) {
+    s[k++] = token;
+    token = strtok(NULL, " ");
+  }
   return 0;
 }
